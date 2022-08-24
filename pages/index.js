22735df1385e138be062d8db6d7cde7f0307e3cf
yuanx <1220998011@qq.com>
2022-08-24 14:51:13 +0800

    chore: update

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
     <div className='content'>
-        <h1>Hi, I am yuanx.</h1>
-        <p>I am a front-end Engineer, I used React in work, But i also like Vue.</p>
-        <p>My ideal is to become an independent developer, I have a side project: <a href='https://m-calendar.yuanx.me' target='_blank' rel="noreferrer">米历</a>, It is a calendar app support chinese holiday, You can download it in AppStore.</p>
-        <p>If you like me you can following me in <a href='https://www.github.com/yunying1' target='_blank' rel="noreferrer">GitHub</a> or <a href='https://twitter.com/yyuan_x' target='_blank' rel="noreferrer">Twitter</a>.</p>
-        <p>Email: lonelyuanx@gmail.com</p>
+        <Head>
+            <title>x Yuan</title>
+        </Head>
+        <h1>Hi, I am x Yuan.</h1>
+        <p>{"I'm a front-end engineer and I'm learning English , Usually use React in my work, but I also like Vue."}</p>
+        <p>I like open source and building anything. I focus on my ideas and turn them into reality.</p>
+        <p>My ideal is to become an independent developer, I have a side project: <a href='https://m-calendar.yuanx.me' target='_blank' rel="noreferrer">米历</a>, It is a calendar app support Chinese holiday, You can download it in AppStore.</p>
+        <p>My other projects: <a href='https://www.github.com/yunying1/worth' target='_blank' rel="noreferrer">Worth</a>.</p>
+        <p>You can following me on <a href='https://www.github.com/yunying1' target='_blank' rel="noreferrer">GitHub</a>, <a href='https://twitter.com/yyuan_x' target='_blank' rel="noreferrer">Twitter</a>, <a href='https://www.zhihu.com/people/yun-ying-37-92' target='_blank' rel="noreferrer">知乎</a> or <a href='https://juejin.cn/user/4292141022723207' target='_blank' rel="noreferrer">掘金</a>.</p>
+        <p>Mail me at <a href="mailto:lonelyuanx@gmail.com">lonelyuanx@gmail.com</a>.</p>
+        <p className="important"><a href="mailto:lonelyuanx@gmail.com">I{"'"}m waiting for a job offer.</a></p>
     </div>
   )
 }
