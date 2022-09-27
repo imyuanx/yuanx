a0eb64ad301c50d38a09e5e5a22be17b40d9b737
yuanx <1220998011@qq.com>
2022-09-27 16:51:52 +0800

    feat: add ackee

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
+import { create as ackeeTrackerCreate } from 'ackee-tracker';
 import '../styles/globals.css';
 import '../styles/tailwind.css';
 import { initTheme } from '../common/useTheme';
@@ -7,6 +8,7 @@ import { initTheme } from '../common/useTheme';
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     initTheme();
+    ackeeTrackerCreate('https://ackee.yuanx.me').record('cff959e0-1e4b-4a3e-b91b-5ffd06b6c870')
   }, []);
 
   return <Component {...pageProps} />;
