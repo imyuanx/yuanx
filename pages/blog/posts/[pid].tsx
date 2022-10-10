529b9d5c7bd883940f92f292208e3fcd5c942e48
yuanx <1220998011@qq.com>
2022-10-10 09:22:53 +0800

    feat: add next page entry

--- pages/blog/posts/[pid].tsx
+++ pages/blog/posts/[pid].tsx
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
+import articleEnv from '../../../posts/posts.json';
 import Nav from '../../../components/Nav';
+import Link from 'next/link';
+
+interface NextPost {
+  id: 0;
+  title: '';
+  date: '';
+  postId: '';
+}
 
 const Article: NextPage = () => {
   const router = useRouter();
   const { pid } = router.query;
   const [markdown, setMarkdown] = useState('');
+  const nextPost = useRef<NextPost | null>(null);
 
   useEffect(() => {
     if (pid) {
       const postPath = require.context(`../../../posts/`);
       const result = postPath(`./${pid}.md`);
       setMarkdown(result.default);
+      let nextPostInfo = getNextPost();
+      nextPost.current = nextPostInfo;
     }
   }, [pid]);
+
+  /**
+   * @desc 获取下一篇文章
+   */
+  const getNextPost = (): NextPost | null => {
+    const curPost = articleEnv.ARTICLES.find((item) => item.postId === pid);
+    if (!curPost) return null;
+    const nextPost = articleEnv.ARTICLES.find(
+      (item) => item.id === curPost.id + 1,
+    );
+    if (!nextPost) return null;
+    return nextPost as NextPost;
+  };
 
   return (
     <div className="container-article w-full h-full flex justify-center content-center">
@@ -50,6 +75,18 @@ const Article: NextPage = () => {
             {markdown}
           </ReactMarkdown>
         </article>
+        <div className="mt-30px flex">
+          <span>下一篇：</span>
+          {nextPost.current?.postId ? (
+            <Link href={nextPost.current?.postId}>
+              <a>{nextPost.current?.title}</a>
+            </Link>
+          ) : (
+            <div>
+              <a>已经是最后一篇了哦</a>
+            </div>
+          )}
+        </div>
         <footer className="text-center mt-30px">
           <a
             className="inline-block no-underline whitespace-nowrap text-[#121314] leading-normal bg-wave bg-16px bg-0px-bottom bg-repeat-x hover:animate-wave dark:!text-white"
@@ -59,7 +96,7 @@ const Article: NextPage = () => {
           >
             CC BY-NC-SA 4.0
           </a>
-          <span className='text-#808080'>{' 2022-PRESENT © x Yuan'}</span>
+          <span className="text-#808080">{' 2022-PRESENT © x Yuan'}</span>
         </footer>
       </main>
     </div>
