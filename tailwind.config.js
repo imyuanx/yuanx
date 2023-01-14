c69744fb2a9453a44c3f07c750b7fa636eacc13b
yuanx <1220998011@qq.com>
2023-01-14 11:51:16 +0800

    fix: Fix config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+const path = require('path');
+
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    path.join(__dirname, './pages/**/*.{js,ts,jsx,tsx}'),
+    path.join(__dirname, './components/**/*.{js,ts,jsx,tsx}'),
   ],
   theme: {
     extend: {
