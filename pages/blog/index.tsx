95f98d396ad58fa2f195f15851d033c48e43366d
yuanx <1220998011@qq.com>
2023-01-22 21:34:21 +0800

    feat: New website title

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 import Link from 'next/link';
 import articleEnv from '../../posts/posts.json';
 
@@ -6,29 +7,34 @@ const ARTICLES = articleEnv.ARTICLES.sort((cur, last) => last.id - cur.id);
 
 const Blog: NextPage = () => {
   return (
-    <main className="box-content pt-[60px] pr-7 pb-7 pl-7 max-w-[650px] min-w-[330px] w-full mx-auto my-0">
-      <h1 className="text-[2em] font-bold mb-[40px] mt-[0.67em]">My Blog</h1>
-      <article>
-        <ul className="list-none p-0">
-          {ARTICLES.map((articleItem) => {
-            return (
-              <li className="mb-[30px]" key={articleItem.postId}>
-                <Link href={`/blog/posts/${articleItem.postId}`}>
-                  <a className="no-underline">
-                    <span className="article-title no-underline text-[18px] text-[#121314] mr-[10px] hover:text-[#000000] hover:underline dark:!text-white">
-                      {articleItem.title}
-                    </span>
-                    <span className="article-date text-[16px] text-[#808080] italic">
-                      {articleItem.date}
-                    </span>
-                  </a>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </article>
-    </main>
+    <>
+      <Head>
+        <title>x Yuan | Blog</title>
+      </Head>
+      <main className="box-content pt-[60px] pr-7 pb-7 pl-7 max-w-[650px] min-w-[330px] w-full mx-auto my-0">
+        <h1 className="text-[2em] font-bold mb-[40px] mt-[0.67em]">My Blog</h1>
+        <article>
+          <ul className="list-none p-0">
+            {ARTICLES.map((articleItem) => {
+              return (
+                <li className="mb-[30px]" key={articleItem.postId}>
+                  <Link href={`/blog/posts/${articleItem.postId}`}>
+                    <a className="no-underline">
+                      <span className="article-title no-underline text-[18px] text-[#121314] mr-[10px] hover:text-[#000000] hover:underline dark:!text-white">
+                        {articleItem.title}
+                      </span>
+                      <span className="article-date text-[16px] text-[#808080] italic">
+                        {articleItem.date}
+                      </span>
+                    </a>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </article>
+      </main>
+    </>
   );
 };
 
--- pages/blog/posts/[pid].tsx
+++ pages/blog/posts/[pid].tsx
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react';
 import type { NextPage } from 'next';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
-import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import rehypeRaw from 'rehype-raw';
@@ -32,82 +32,106 @@ const Article: NextPage = () => {
   }, [pid]);
 
   /**
-   * @desc 获取下一篇文章
+   * @desc Get the env of article by `postId`
+   * @param {String} pid article id
+   */
+  const getPostEnv = (pid: String): NextPost | null => {
+    const post = articleEnv.ARTICLES.find((item) => item.postId === pid);
+    if (!post) return null;
+    return post as NextPost;
+  };
+
+  /**
+   * @desc Get the next article
    */
   const getNextPost = (): NextPost | null => {
-    const curPost = articleEnv.ARTICLES.find((item) => item.postId === pid);
+    if (!pid || typeof pid !== 'string') return null;
+    const curPost = getPostEnv(pid);
     if (!curPost) return null;
-    const nextPost = articleEnv.ARTICLES.find(
-      (item) => item.id === curPost.id + 1,
-    );
+    const nextPost = getPostEnv(`${curPost.id + 1}`);
     if (!nextPost) return null;
     return nextPost as NextPost;
   };
 
+  /**
+   * @desc Get the title of the current article
+   */
+  const getCurPostTitle = (): string => {
+    if (!pid || typeof pid !== 'string') return '';
+    const curPost = getPostEnv(pid);
+    if (!curPost) return '';
+    return curPost.title;
+  };
+
   return (
-    <div className="container-article w-full h-full flex justify-center content-center">
-      <main className="box-border article-content max-w-[900px] w-full h-full pt-[60px] pr-[28px] pb-[28px] pl-[28px]">
-        <article className="article">
-          <ReactMarkdown
-            rehypePlugins={[rehypeRaw]}
-            components={{
-              h1: ({ children }) => (
-                <h1 className="text-[2em] leading-[1.2] font-bold mb-[0.67em] mt-[0.67em]">
-                  {children}
-                </h1>
-              ),
-              p: ({ children }) => {
-                return (
-                  <p className="text-[18px] leading-[1.8] mt-[1em]">
+    <>
+      <Head>
+        <title>{getCurPostTitle()}</title>
+      </Head>
+      <div className="container-article w-full h-full flex justify-center content-center">
+        <main className="box-border article-content max-w-[900px] w-full h-full pt-[60px] pr-[28px] pb-[28px] pl-[28px]">
+          <article className="article">
+            <ReactMarkdown
+              rehypePlugins={[rehypeRaw]}
+              components={{
+                h1: ({ children }) => (
+                  <h1 className="text-[2em] leading-[1.2] font-bold mb-[0.67em] mt-[0.67em]">
                     {children}
-                  </p>
-                );
-              },
-              a: ({ children, href }) => (
-                <a
-                  className="inline-block no-underline whitespace-nowrap text-[#121314] leading-normal bg-wave bg-16px bg-0px-bottom bg-repeat-x hover:animate-wave dark:!text-white"
-                  href={href}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  {children}
-                </a>
-              ),
-              code: ({ children }) => (
-                <code className="relative mx-[4px] rounded-[6px] after:content-[''] after:absolute after:z-[-1] after:top-[66%] after:left-[-0.1em] after:right-[-0.1em] after:bottom-[0] after:bg-[#ebebeb]">
-                  {children}
-                </code>
-              ),
-            }}
-          >
-            {markdown}
-          </ReactMarkdown>
-        </article>
-        <div className="mt-[30px] flex">
-          <span>下一篇：</span>
-          {nextPost.current?.postId ? (
-            <Link href={nextPost.current?.postId}>
-              <a>{nextPost.current?.title}</a>
-            </Link>
-          ) : (
-            <div>
-              <a>已经是最后一篇了哦</a>
-            </div>
-          )}
-        </div>
-        <footer className="text-center mt-[30px]">
-          <a
-            className="inline-block no-underline whitespace-nowrap text-[#121314] leading-normal bg-wave bg-16px bg-0px-bottom bg-repeat-x hover:animate-wave dark:!text-white"
-            target="_blank"
-            href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
-            rel="noreferrer"
-          >
-            CC BY-NC-SA 4.0
-          </a>
-          <span className="text-[#808080]">{' 2022-PRESENT © x Yuan'}</span>
-        </footer>
-      </main>
-    </div>
+                  </h1>
+                ),
+                p: ({ children }) => {
+                  return (
+                    <p className="text-[18px] leading-[1.8] mt-[1em]">
+                      {children}
+                    </p>
+                  );
+                },
+                a: ({ children, href }) => (
+                  <a
+                    className="inline-block no-underline whitespace-nowrap text-[#121314] leading-normal bg-wave bg-16px bg-0px-bottom bg-repeat-x hover:animate-wave dark:!text-white"
+                    href={href}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {children}
+                  </a>
+                ),
+                code: ({ children }) => (
+                  <code className="relative mx-[4px] rounded-[6px] after:content-[''] after:absolute after:z-[-1] after:top-[66%] after:left-[-0.1em] after:right-[-0.1em] after:bottom-[0] after:bg-[#ebebeb]">
+                    {children}
+                  </code>
+                ),
+              }}
+            >
+              {markdown}
+            </ReactMarkdown>
+          </article>
+          <div className="mt-[30px] flex">
+            <span>下一篇：</span>
+            {nextPost.current?.postId ? (
+              <Link href={nextPost.current?.postId}>
+                <a>{nextPost.current?.title}</a>
+              </Link>
+            ) : (
+              <div>
+                <a>已经是最后一篇了哦</a>
+              </div>
+            )}
+          </div>
+          <footer className="text-center mt-[30px]">
+            <a
+              className="inline-block no-underline whitespace-nowrap text-[#121314] leading-normal bg-wave bg-16px bg-0px-bottom bg-repeat-x hover:animate-wave dark:!text-white"
+              target="_blank"
+              href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
+              rel="noreferrer"
+            >
+              CC BY-NC-SA 4.0
+            </a>
+            <span className="text-[#808080]">{' 2022-PRESENT © x Yuan'}</span>
+          </footer>
+        </main>
+      </div>
+    </>
   );
 };
 
