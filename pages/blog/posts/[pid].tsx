380bd31147f66d5c6b324cc7efd7aa951f9356de
yuanx <yuanx.me@qq.com>
2023-04-01 23:03:06 +0800

    style: Clean log

--- pages/blog/posts/[pid].tsx
+++ pages/blog/posts/[pid].tsx
@@ -116,7 +116,6 @@ const Article: NextPage = () => {
                 ),
                 code({ node, inline, className, children, style, ...props }) {
                   const match = /language-(\w+)/.exec(className || '');
-                  console.log('match', match, className, inline);
                   return !inline && match ? (
                     <>
                       <SyntaxHighlighter
