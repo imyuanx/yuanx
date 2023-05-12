103e978dc0863b14986126854018f229bc89b85d
yuanx <yuanx.me@qq.com>
2023-05-12 16:50:59 +0800

    feat: add avatar for OG image

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -1,10 +1,13 @@
 import { ImageResponse } from '@vercel/og';
+import { NextRequest } from 'next/server';
 
 export const config = {
   runtime: 'edge',
 };
 
-export default function handler() {
+export default function handler(req: NextRequest) {
+  const origin = req.nextUrl.origin;
+
   return new ImageResponse(
     (
       <div
@@ -22,6 +25,7 @@ export default function handler() {
           fontSize: 24,
           fontStyle: 'normal',
           color: 'black',
+          // filter: 'invert(.92)', // TODO: dark mode
         }}
       >
         <div
@@ -32,7 +36,26 @@ export default function handler() {
             width: '60%',
           }}
         >
-          <b style={{ fontSize: 40 }}>yuanx</b>
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              height: 60,
+              gap: 12,
+            }}
+          >
+            <img
+              src={`${origin}/avatar.jpg`}
+              alt="avatar"
+              style={{
+                borderRadius: '50%',
+                width: 60,
+                height: 60,
+                filter: 'invert(0)',
+              }}
+            />
+            <b style={{ fontSize: 40 }}>yuanx</b>
+          </div>
           <p>
             {/*
               // TODO: https://github.com/vercel/satori/issues/470
