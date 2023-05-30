79bd465daadd71082fe5244d55e2aad7a5170bed
yuanx <yuanx.me@qq.com>
2023-05-30 22:09:04 +0800

    feat: add tips for theme icon

--- src/components/Icon/system.tsx
+++ src/components/Icon/system.tsx
@@ -1,20 +1,28 @@
+import { Tooltip } from 'react-tooltip';
+import 'react-tooltip/dist/react-tooltip.css';
+
 function SystemIcon() {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="18"
-      height="18"
-      viewBox="0 0 24 24"
-    >
-      <path
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="1.6"
-        d="M2 6a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6Zm6 14h8"
-      />
-    </svg>
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="18"
+        height="18"
+        viewBox="0 0 24 24"
+        data-tooltip-id="system-theme-tips"
+        data-tooltip-content="Follow System Theme"
+      >
+        <path
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.6"
+          d="M2 6a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6Zm6 14h8"
+        />
+      </svg>
+      <Tooltip id="system-theme-tips" className="rounded-[5px]" />
+    </>
   );
 }
 
