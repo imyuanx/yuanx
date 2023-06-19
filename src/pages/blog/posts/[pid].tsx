bf0df5ae69ba1dc43e140d327d4f761d4d439319
yuanx <yuanx.me@qq.com>
2023-06-19 11:15:37 +0800

    perf: separate function from component

--- src/pages/blog/posts/[pid].tsx
+++ src/pages/blog/posts/[pid].tsx
@@ -3,7 +3,7 @@ import type { GetStaticPaths, NextPage } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
-import ReactMarkdown from 'react-markdown';
+import ReactMarkdown, { Components } from 'react-markdown';
 import rehypeRaw from 'rehype-raw';
 import Link from 'next/link';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
@@ -53,103 +53,110 @@ const getPostTitle = (pid: string): string => {
   return curPost.title;
 };
 
+const reactMarkdownComponents = {
+  h1: ({ children }) => (
+    <h1 className="text-[2.5em] leading-[1.2] font-bold mb-[0.67em] mt-[0.67em]">
+      {children}
+    </h1>
+  ),
+  h2: ({ children }) => <h1 className="text-[2em] font-bold">{children}</h1>,
+  h3: ({ children }) => <h1 className="text-[1.8em] font-bold">{children}</h1>,
+  h4: ({ children }) => <h1 className="text-[1.6em] font-bold">{children}</h1>,
+  p: ({ children }) => {
+    return <p className="text-[18px] leading-[1.8] mt-[1em]">{children}</p>;
+  },
+  a: ({ children, href }) => (
+    <a
+      className="inline-block no-underline whitespace-nowrap text-[#121314] leading-normal bg-wave bg-16px bg-0px-bottom bg-repeat-x hover:animate-wave dark:!text-white"
+      href={href}
+      target="_blank"
+      rel="noreferrer"
+    >
+      {children}
+    </a>
+  ),
+  code({ node, inline, className, children, style, ...props }) {
+    const match = /language-(\w+)/.exec(className || '');
+    return !inline && match ? (
+      <>
+        <SyntaxHighlighter
+          className={'rounded max-w-pre-mobile block dark:hidden'}
+          style={oneLight}
+          language={match[1]}
+          PreTag="div"
+          {...props}
+        >
+          {String(children).replace(/\n$/, '')}
+        </SyntaxHighlighter>
+        <SyntaxHighlighter
+          className={'rounded max-w-pre-mobile hidden dark:block'}
+          style={oneDark}
+          language={match[1]}
+          PreTag="div"
+          {...props}
+        >
+          {String(children).replace(/\n$/, '')}
+        </SyntaxHighlighter>
+      </>
+    ) : (
+      <code className="relative mx-[4px] rounded-[6px] whitespace-nowrap after:content-[''] after:absolute after:z-[-1] after:top-[66%] after:left-[-0.1em] after:right-[-0.1em] after:bottom-[0] after:bg-[#ebebeb] after:dark:bg-[#333333]">
+        {children}
+      </code>
+    );
+  },
+  blockquote: ({ children }) => (
+    <blockquote className="border-0 border-l-4 border-solid border-l-[#ebebeb] m-0 pl-[16px] text-[#555555] dark:text-[#acacac] dark:border-l-[#7a7a7a]">
+      {children}
+    </blockquote>
+  ),
+  ul: ({ children }) => (
+    <ul className="flex flex-col gap-[10px] text-[18px]">{children}</ul>
+  ),
+  li: ({ children }) => <li className="leading-[1.6]">{children}</li>,
+} as Components;
+
+function Footer() {
+  return (
+    <footer className="text-center mt-[30px]">
+      <a
+        className="inline-block no-underline whitespace-nowrap text-[#121314] leading-normal bg-wave bg-16px bg-0px-bottom bg-repeat-x hover:animate-wave dark:!text-white"
+        target="_blank"
+        href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
+        rel="noreferrer"
+      >
+        CC BY-NC-SA 4.0
+      </a>
+      <span className="text-[#808080]">{' 2022-PRESENT © x Yuan'}</span>
+    </footer>
+  );
+}
+
 const Article: NextPage<{ postMarkdown: string }> = ({ postMarkdown }) => {
   const router = useRouter();
   const { pid } = router.query;
   const [nextPost, setNextPost] = useState<NextPost | null>(null);
+  const [title, setTitle] = useState('');
 
   useEffect(() => {
     if (pid && typeof pid === 'string') {
-      const nextPostInfo = getNextPost(pid);
-      setNextPost(nextPostInfo);
+      const nextPost = getNextPost(pid);
+      const title = getPostTitle(pid);
+      setNextPost(nextPost);
+      setTitle(title);
     }
   }, [pid]);
 
   return (
     <>
       <Head>
-        <title>{getPostTitle(pid as string)}</title>
+        <title>{title}</title>
       </Head>
       <div className="container-article w-full h-full flex justify-center content-center">
         <main className="box-border article-content max-w-[900px] w-full h-full pt-[60px] pr-[28px] pb-[28px] pl-[28px]">
           <article className="article">
             <ReactMarkdown
               rehypePlugins={[rehypeRaw]}
-              components={{
-                h1: ({ children }) => (
-                  <h1 className="text-[2.5em] leading-[1.2] font-bold mb-[0.67em] mt-[0.67em]">
-                    {children}
-                  </h1>
-                ),
-                h2: ({ children }) => (
-                  <h1 className="text-[2em] font-bold">{children}</h1>
-                ),
-                h3: ({ children }) => (
-                  <h1 className="text-[1.8em] font-bold">{children}</h1>
-                ),
-                h4: ({ children }) => (
-                  <h1 className="text-[1.6em] font-bold">{children}</h1>
-                ),
-                p: ({ children }) => {
-                  return (
-                    <p className="text-[18px] leading-[1.8] mt-[1em]">
-                      {children}
-                    </p>
-                  );
-                },
-                a: ({ children, href }) => (
-                  <a
-                    className="inline-block no-underline whitespace-nowrap text-[#121314] leading-normal bg-wave bg-16px bg-0px-bottom bg-repeat-x hover:animate-wave dark:!text-white"
-                    href={href}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {children}
-                  </a>
-                ),
-                code({ node, inline, className, children, style, ...props }) {
-                  const match = /language-(\w+)/.exec(className || '');
-                  return !inline && match ? (
-                    <>
-                      <SyntaxHighlighter
-                        className={'rounded max-w-pre-mobile block dark:hidden'}
-                        style={oneLight}
-                        language={match[1]}
-                        PreTag="div"
-                        {...props}
-                      >
-                        {String(children).replace(/\n$/, '')}
-                      </SyntaxHighlighter>
-                      <SyntaxHighlighter
-                        className={'rounded max-w-pre-mobile hidden dark:block'}
-                        style={oneDark}
-                        language={match[1]}
-                        PreTag="div"
-                        {...props}
-                      >
-                        {String(children).replace(/\n$/, '')}
-                      </SyntaxHighlighter>
-                    </>
-                  ) : (
-                    <code className="relative mx-[4px] rounded-[6px] whitespace-nowrap after:content-[''] after:absolute after:z-[-1] after:top-[66%] after:left-[-0.1em] after:right-[-0.1em] after:bottom-[0] after:bg-[#ebebeb] after:dark:bg-[#333333]">
-                      {children}
-                    </code>
-                  );
-                },
-                blockquote: ({ children }) => (
-                  <blockquote className="border-0 border-l-4 border-solid border-l-[#ebebeb] m-0 pl-[16px] text-[#555555] dark:text-[#acacac] dark:border-l-[#7a7a7a]">
-                    {children}
-                  </blockquote>
-                ),
-                ul: ({ children }) => (
-                  <ul className="flex flex-col gap-[10px] text-[18px]">
-                    {children}
-                  </ul>
-                ),
-                li: ({ children }) => (
-                  <li className="leading-[1.6]">{children}</li>
-                ),
-              }}
+              components={reactMarkdownComponents}
             >
               {postMarkdown}
             </ReactMarkdown>
@@ -164,17 +171,7 @@ const Article: NextPage<{ postMarkdown: string }> = ({ postMarkdown }) => {
               </div>
             )}
           </div>
-          <footer className="text-center mt-[30px]">
-            <a
-              className="inline-block no-underline whitespace-nowrap text-[#121314] leading-normal bg-wave bg-16px bg-0px-bottom bg-repeat-x hover:animate-wave dark:!text-white"
-              target="_blank"
-              href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
-              rel="noreferrer"
-            >
-              CC BY-NC-SA 4.0
-            </a>
-            <span className="text-[#808080]">{' 2022-PRESENT © x Yuan'}</span>
-          </footer>
+          <Footer />
         </main>
       </div>
     </>
