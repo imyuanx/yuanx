7080115e156e98b8a1f6b67e25b944a0a754fb9a
yuanx <yuanx.me@qq.com>
2023-07-28 17:21:28 +0800

    feat: add animation to DuolingoCard

--- src/components/DuolingoLogo/index.tsx
+++ src/components/DuolingoLogo/index.tsx
@@ -1,5 +1,6 @@
 import dynamic from 'next/dynamic';
 import DuolingoLogoIcon from '@/icons/duolingo-logo.svg';
+import { animated, useSpringValue } from 'react-spring';
 import { Tooltip } from 'react-tooltip';
 import 'react-tooltip/dist/react-tooltip.css';
 
@@ -8,6 +9,19 @@ const DuolingoCard = dynamic(() => import('@/components/DuolingoCard'), {
 });
 
 function DuolingoLogo() {
+  const height = useSpringValue(0, { config: { duration: 200 } });
+  const opacity = useSpringValue(0.5, { config: { duration: 200 } });
+
+  function animatedStart() {
+    height.start(254);
+    opacity.start(1);
+  }
+
+  function animatedReset() {
+    height.start(0);
+    opacity.start(0.5);
+  }
+
   return (
     <>
       <DuolingoLogoIcon
@@ -17,9 +31,16 @@ function DuolingoLogo() {
       />
       <Tooltip
         anchorSelect={'#duolingo-card'}
-        className="pointer-events-auto bg-[transparent] p-0 opacity-100 shadow-[0px_0px_10px_rgba(0,0,0,0.12)]"
+        className="h-[254px] pointer-events-auto bg-[transparent] p-0 opacity-100"
+        afterShow={animatedStart}
+        afterHide={animatedReset}
       >
-        <DuolingoCard />
+        <animated.div
+          style={{ height, opacity }}
+          className="shadow-[0px_0px_10px_rgba(0,0,0,0.12)] overflow-hidden"
+        >
+          <DuolingoCard />
+        </animated.div>
       </Tooltip>
     </>
   );
