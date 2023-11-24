339b28416bd17bf1de84c699dbf36aa06277fc65
yuanx <yuanx.me@qq.com>
2023-11-24 22:54:43 +0800

    refactor: refactor form

--- src/components/AddNote/index.tsx
+++ src/components/AddNote/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from 'react';
+import { useState } from 'react';
 import useNotes from '@/common/useNotes';
 import { Button } from '@/components/ui/button';
 import {
@@ -10,46 +10,51 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import AddIcon from '@/icons/add.svg';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+
+const formSchema = z.object({
+  title: z.string().refine((v) => !!v, 'Title is required'),
+  content: z.string().refine((v) => !!v, 'Content is required'),
+});
 
 function AddNote() {
   const [open, setOpen] = useState(false);
-  const [noteData, setNoteData] = useState({ title: '', content: '' });
   const { addNote, mutate } = useNotes();
 
-  const onConfirm = () => {
-    const { title, content } = noteData; // TODO: check empty
-    addNote(title, content, 0, 0).then((res) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: '',
+      content: '',
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    const { title, content } = values;
+    addNote(title, content, 0, 0).then(() => {
       mutate();
       setOpen(false);
     });
   };
 
-  const onAddNote = () => {
-    setOpen(false);
-  };
-
-  const onCancel = () => {
-    setOpen(false);
-  };
-
-  const onTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setNoteData({ ...noteData, title: e.currentTarget.value });
-  };
-
-  const onContentChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setNoteData({ ...noteData, content: e.currentTarget.value });
-  };
+  const onCancel = () => setOpen(false);
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
-        <AddIcon
-          className="mt-[2px] w-6 cursor-pointer text-[#808080] hover:text-black dark:hover:text-white"
-          onClick={onAddNote}
-        />
+        <AddIcon className="mt-0.5 w-6 cursor-pointer text-[#808080] hover:text-black dark:hover:text-white" />
       </DialogTrigger>
       <DialogContent className="rounded-lg sm:max-w-[425px]">
         <DialogHeader>
@@ -58,28 +63,44 @@ function AddNote() {
             Once published, it cannot be edited or deleted
           </DialogDescription>
         </DialogHeader>
-        <div className="flex flex-col gap-4 py-4">
-          <Input
-            id="title"
-            placeholder="Title"
-            className="w-full"
-            onChange={onTitleChange}
-          />
-          <Textarea
-            id="content"
-            placeholder="Just write something..."
-            className="w-full min-h-[120px]"
-            onChange={onContentChange}
-          />
-        </div>
-        <DialogFooter className="flex-col space-y-2 sm:space-y-0">
-          <Button type="button" variant="outline" onClick={onCancel}>
-            Cancel
-          </Button>
-          <Button type="submit" onClick={onConfirm}>
-            Confirm
-          </Button>
-        </DialogFooter>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="Title" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="content"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Textarea
+                      placeholder="Just write something..."
+                      className="w-full min-h-[120px]"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter className="flex-col space-y-2 sm:space-y-0">
+              <Button type="button" variant="outline" onClick={onCancel}>
+                Cancel
+              </Button>
+              <Button type="submit">Confirm</Button>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
