0f4c5dc80e33c0feded2a19e2c22efba44e02a40
yuanx <yuanx.me@qq.com>
2023-12-02 21:09:17 +0800

    style: remove type

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,10 +21,9 @@ import { PROJECTS_ITEM_TYPE } from '@/pages/projects';
 import axios from 'axios';
 import clsx from 'clsx';
 
-const CLASS_A: string =
+const CLASS_A =
   'inline-block no-underline whitespace-nowrap text-[#121314] leading-normal bg-wave bg-16px bg-0px-bottom bg-repeat-x hover:animate-wave dark:!text-white';
-const CLASS_P: string =
-  'text-[1.6em] font-light max-w-[28em] leading-[1.4] mt-[1em]';
+const CLASS_P = 'text-[1.6em] font-light max-w-[28em] leading-[1.4] mt-[1em]';
 
 const SOCIAL_MEDIA = [
   {
