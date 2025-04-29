581914fac88910868ec9c0749b52f6802993fa0e
yuanx <50652358+imyuanx@users.noreply.github.com>
2025-04-29 13:14:42 +0800

    feat: simplified theme system (#95)

--- src/components/ThemeMode/index.tsx
+++ src/components/ThemeMode/index.tsx
@@ -1,36 +1,23 @@
 import { useEffect, useState } from 'react';
-import MoonIcon from '@/icons/moon.svg';
-import SunIcon from '@/icons/sun.svg';
-import SystemIcon from '@/icons/system.svg';
+
 import clsx from 'clsx';
 import { useTheme } from 'next-themes';
-import { Tooltip } from 'react-tooltip';
-import 'react-tooltip/dist/react-tooltip.css';
+
+import MoonIcon from '@/icons/moon.svg';
+import SunIcon from '@/icons/sun.svg';
 
 const TEXT_COLOR =
   'text-[#808080] hover:text-black dark:text-[#a1a1a1] dark:hover:!text-white';
 
 function ThemeMode() {
-  const { theme, setTheme } = useTheme();
+  const { resolvedTheme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
   /**
    * @desc Change theme
    */
   const onClickHandle = () => {
-    let nextTheme = 'system';
-    switch (theme) {
-      case 'system':
-      case 'undefined':
-        nextTheme = 'light';
-        break;
-      case 'light':
-        nextTheme = 'dark';
-        break;
-      case 'dark':
-        nextTheme = 'system';
-        break;
-    }
+    const nextTheme = resolvedTheme === 'light' ? 'dark' : 'light';
     setTheme(nextTheme);
   };
 
@@ -47,17 +34,7 @@ function ThemeMode() {
       onClick={onClickHandle}
       className={clsx('flex items-center', TEXT_COLOR)}
     >
-      {theme === 'dark' && <MoonIcon />}
-      {theme === 'light' && <SunIcon />}
-      {(theme === 'system' || theme === 'undefined') && (
-        <>
-          <SystemIcon
-            data-tooltip-id="system-theme-tips"
-            data-tooltip-content="Follow System Theme"
-          />
-          <Tooltip id="system-theme-tips" className="rounded-[5px]" />
-        </>
-      )}
+      {resolvedTheme === 'dark' ? <MoonIcon /> : <SunIcon />}
     </a>
   );
 }
